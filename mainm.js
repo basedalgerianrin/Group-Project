495c16e5d822a33839551a7bfa86fee41bd7c55e
Sami Hammar <G00393324@atu.ie>
2024-12-15 19:48:42 +0000

    Changes

--- mainm.js
+++ mainm.js
@@ -1,33 +1,31 @@
 function goToPage(page) {
     window.location.href = page;
 }
-
+document.getElementById("full").style.display = "none";
+document.getElementById("limited").style.display = "none";
+document.getElementById("fail").style.display = "none";
+document.getElementById("fail").style.display="none";
 
 function offerChecker() {
     var offer = document.getElementById("offer").value;
-
-    document.getElementById("full").style.display = "none";
-    document.getElementById("limited").style.display = "none";
-    document.getElementById("forbid").style.display = "none";
-
     if (offer >= 50) {
         document.getElementById("full").style.display = "block";
         document.getElementById("limited").style.display = "none";
-        document.getElementById("forbid").style.display = "none";
+        document.getElementById("fail").style.display = "none";
         document.getElementById("full").style.background = "green"
 
 
 
-    } else if (offer < 50 && age >= 21) {
+    } else if (offer < 50 && offer >= 21) {
         document.getElementById("limited").style.display = "block";
         document.getElementById("full").style.display = "none";
-        document.getElementById("forbid").style.display = "none";
+        document.getElementById("fail").style.display = "none";
         document.getElementById("limited").style.background = "orange"
 
     } else {
         document.getElementById("full").style.display = "none";
         document.getElementById("limited").style.display = "none";
-        document.getElementById("forbid").style.display = "block";
-        document.getElementById("forbid").style.background = "red"
+        document.getElementById("fail").style.display = "block";
+        document.getElementById("fail").style.background = "red"
     }
 }
