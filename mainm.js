70536e697ac2e4325163405909e0a584a12f6014
Sami Hammar - STUDENT <g00393324@atu.ie>
2024-12-16 11:05:19 +0000

    changes made

--- mainm.js
+++ mainm.js
@@ -1,15 +1,15 @@
-function goToPage(page) {
-    window.location.href = page;
-}
-document.getElementById("full").style.display = "none";
-document.getElementById("limited").style.display = "none";
-document.getElementById("fail").style.display = "none";
-document.getElementById("fail").style.display="none";
+
 
 function offerChecker() {
+
+    document.getElementById("half").style.display = "none";
+    document.getElementById("limited").style.display = "none";
+    document.getElementById("fail").style.display = "none";
+    document.getElementById("fail").style.display = "none";
+
     var offer = document.getElementById("offer").value;
     if (offer >= 50) {
-        document.getElementById("full").style.display = "block";
+        document.getElementById("half").style.display = "block";
         document.getElementById("limited").style.display = "none";
         document.getElementById("fail").style.display = "none";
         document.getElementById("full").style.background = "green"
@@ -18,14 +18,14 @@ function offerChecker() {
 
     } else if (offer < 50 && offer >= 21) {
         document.getElementById("limited").style.display = "block";
-        document.getElementById("full").style.display = "none";
+        document.getElementById("half").style.display = "none";
         document.getElementById("fail").style.display = "none";
-        document.getElementById("limited").style.background = "orange"
+        document.getElementById("limited").style.background = "none";
 
     } else {
-        document.getElementById("full").style.display = "none";
+        document.getElementById("half").style.display = "none";
         document.getElementById("limited").style.display = "none";
         document.getElementById("fail").style.display = "block";
-        document.getElementById("fail").style.background = "red"
+        document.getElementById("fail").style.background = "none";
     }
 }
